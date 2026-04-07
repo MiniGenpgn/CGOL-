2f380fceccdf3b1bc3bfd38c9711d432dd059845
MiniGenpgn <20265045@nvnet.org>
2026-04-07 07:58:41 -0400

    Add grid simulation with cell life cycle management
    Implement a grid-based simulation with cell aging and random initialization.

---
+++ main3_verison.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+int grid[50][50];
+int grid2[50][50];
+int gridAge[50][50];
+int i,j, Lcount, size;
+
+void fill(void);
+void print(void);
+void change(void);
+void rng(void);
+void clear(void);
+void wait(void);
+void reprint(void);
+void color(void);
+
+int main()
+{
+    srand(time(NULL));
+    
+    fill();
+    print();
+    rng();
+
+    for(Lcount=0;Lcount<=100;Lcount++)
+    {
+        change();
+        color();
+        wait();
+        clear();
+        reprint();
+        
+        if(Lcount%10==0)
+            rng();
+    }
+    
+    return 0;
+}
+
+void fill(void)
+{
+    printf("Enter board size: "); // user enters board size (size x size)
+    scanf("%i",&size);
+    
+    for(j=0;j<size;j++) // sets each cell
+    {
+        for(i=0;i<size;i++)
+        {
+            grid[j][i]=0; // og grid
+            grid2[j][i]=0; // changed grid
+            gridAge[j][i]=0; // grid for each cell's age
+        }
+    }
+    
+}
+
+void print(void)
+{
+    printf("\n\n---------Loop Counter: %i-----------\n\n",Lcount);
+        
+        for(j=0;j<size;j++) // prints 1st board
+        {
+            for(i=0;i<size;i++)
+            {
+                printf("%i ",grid[j][i]);
+            }
+            
+            printf("\n");
+        }
+}
+
+void reprint(void)
+{
+    printf("\n\n---------Loop Counter: %i-----------\n\n",Lcount);
+    
+    for(j=0;j<size;j++)
+    {
+        for(i=0;i<size;i++)
+        {
+            grid[j][i]=grid2[j][i]; // sets changed board to og board
+        }
+    }
+    
+    // reprints board
+    for(j=0;j<size;j++)
+    {
+        for (int dy=0;dy<2;dy++) // prints row twice
+        {
+            for(i=0;i<size;i++)
+            {
+                for (int dx=0;dx<2;dx++) // prints column twice
+                {
+                    if(grid2[j][i]==0) // if cell is dead
+                    {
+                        printf("\e[40m  \e[0m"); //empty space
+                    }
+                        
+                    else if(grid2[j][i]==1) // if cell is alive
+                    {
+                        if(gridAge[j][i]==0)
+                        {
+                            printf("\e[40m  \e[0m");
+                        }
+                        
+                        if(gridAge[j][i]==1) // if cell age is 1
+                        {
+                            printf("\e[42m  \e[0m"); //green
+                        }
+                        
+                        if(gridAge[j][i]==2) // if cell age is 2
+                        {
+                           printf("\e[43m  \e[0m"); //yellow
+                        }
+                        
+                        if(gridAge[j][i]>=3) // if cell age is 3
+                        {
+                            printf("\e[41m  \e[0m"); //red
+                        }
+                    }
+                }
+            }
+            
+            printf("\n");
+        }
+    }
+}
+
+void change(void)
+{
+    for(j=0;j<size;j++)
+    {
+        for(i=0;i<size;i++)
+        {   
+            // checking each cell
+            // 0-8 based on how many are alive
+            
+            int neighbors = grid[j-1][i-1]+grid[j-1][i]+grid[j-1][i+1]+grid[j][i-1]+grid[j][i+1]+grid[j+1][i-1]+grid[j+1][i]+grid[j+1][i+1];;
+            // calculates neighbors
+            
+            if(neighbors>=4 && grid[j][i]==1) // if more than 4 neighbors and it is alive
+                grid2[j][i]=0; // then it dies
+                
+            else if(neighbors<=1 && grid[j][i]==1) // if less than or equal to 1 neighbor and it is alive
+                grid2[j][i]=0; // then it dies
+                
+            else if(neighbors>=2 && neighbors<=3 && grid[j][i]==1) // if neighbors is 2 or 3 and it is alive
+                grid2[j][i]=1; // stay alive
+                
+            else if(neighbors==3 && grid[j][i]==0) // if neighbors is 3 and it is dead
+                grid2[j][i]=1; // come back to life
+        }
+    }
+}
+
+void rng(void)
+{
+    int z=0, randomx, randomy;
+    
+    while(z<size*3) // loop until counter less than three times the size
+    {
+        randomy = rand()%size; // randomize y
+        randomx = rand()%size; // randomize x
+        grid[randomy][randomx]=1; // set that specific random cell to alive
+        z++; // add 1 to counter
+    }
+}
+
+void clear(void)
+{
+	printf("\e[?25l"); 
+    printf("\e[1;1H\e[2J"); // clear console
+}
+
+void wait(void)
+{
+    usleep(120000); // wait
+}
+
+void color(void)
+{
+    for(j=0;j<size;j++)
+    {
+        for(i=0;i<size;i++)
+        {
+            if(grid2[j][i]==1) // if the cell is alive
+                gridAge[j][i]=gridAge[j][i]+1; // add age to cell in age array
+                
+            if(grid2[j][i]==1 && gridAge[j][i]>6)
+                gridAge[j][i]=0;
+        }
+    }
+}
